b079e022d3e691012a21f073283f9a2ec07ad75d
ayadato <ayato_0412t@softbank.ne.jp>
2024-03-10 19:30:07 +0100

    [fix,update]全体的なバグ及び機能修正
    全体的に乱雑だったコードの記述と把握していたバグの修正を行いました。

--- lottery.go
+++ lottery.go
@@ -32,7 +32,7 @@ func NewBuyLottery(file string) *BuyLottery {
 	return ab
 }
 
-// Parse関数: スペースで区切られた数字文字列を[]int に変換
+// スペースで区切られた数字文字列を[]int に変換
 func Parse(input string) []int {
 	var numbers []int
 	for _, strNum := range strings.Fields(input) {
@@ -53,42 +53,36 @@ func (ab *BuyLottery) readLottery() {
 		os.Exit(1)
 	}
 	defer f.Close()
-
 	s := bufio.NewScanner(f)
 	for s.Scan() {
 		numbers := Parse(s.Text())
 		lottery := &Lottery{
 			number: numbers,
 		}
-
 		ab.numbers = append(ab.numbers, lottery)
 	}
-
 	if err := s.Err(); err != nil {
 		fmt.Fprintln(os.Stderr, "エラー：", err)
 		os.Exit(1)
 	}
 }
 
-func (ab *BuyLottery) BuyTicket(screen tcell.Screen, style tcell.Style) {
+func (ab *BuyLottery) BuyTicket(screen tcell.Screen) {
+	style := tcell.StyleDefault.Foreground(tcell.ColorWhite).Bold(true)
 	lottery := &Lottery{
 		number: Rnumbers(),
 	}
 	if chs(lottery.number, ab) {
-		ab.BuyTicket(screen, style)
+		ab.BuyTicket(screen)
 	} else {
 		ab.numbers = append(ab.numbers, lottery)
 		ab.saveLottery()
-		//fmt.Println("ランダムで購入した宝くじ:")
-		//fmt.Println(lottery.number)
-
 		numbersStr := ""
 		for _, num := range lottery.number {
 			numbersStr += strconv.Itoa(num) + " "
 		}
-
-		setContents(screen, 33, 14, "ランダムで購入した宝くじ", tcell.StyleDefault)
-		setContents(screen, 33, 16, numbersStr, tcell.StyleDefault)
+		setContents(screen, 33, 14, "ランダムで購入した宝くじ", style)
+		setContents(screen, 33, 16, numbersStr, style)
 		screen.Show()
 		es := screen.PollEvent()
 		switch es := es.(type) {
@@ -97,117 +91,96 @@ func (ab *BuyLottery) BuyTicket(screen tcell.Screen, style tcell.Style) {
 			case tcell.KeyEscape, tcell.KeyEnter:
 			}
 		}
-		//time.Sleep(3 * time.Second)
 	}
 }
 
-func (ab *BuyLottery) CBuyTicket(screen tcell.Screen, style tcell.Style) {
-	numbers := make([]int, 7)
+func (ab *BuyLottery) CBuyTicket(screen tcell.Screen) {
+	tnumber := make([]int, 7)
+	style := tcell.StyleDefault.Foreground(tcell.ColorWhite).Bold(true)
+	fl := 0
 	for i := 0; i < 7; i++ {
 		screen.Clear()
-		setContents(screen, 33, 10, "数字を選択してください(1~37): ", tcell.StyleDefault)
-		setContents(screen, 33, 12, "1桁の場合、01のように入力", tcell.StyleDefault)
+		style = tcell.StyleDefault.Foreground(tcell.ColorWhite).Bold(true)
+		setContents(screen, 33, 10, "数字を選択してください(1~37): ", style)
+		setContents(screen, 33, 12, "1桁の場合、01のように入力", style)
+		setContents(screen, 19, 14, "選択した数字:", style)
+		//現時点で選択されている数字一覧を表示
+		h := 33
+		for _, number := range tnumber {
+			if number != 0 {
+				pnumber := strconv.Itoa(number)
+				setContents(screen, h, 14, pnumber, style)
+				h += 3
+			}
+		}
 		screen.Show()
-
-		// 1桁目の数字を取得
 		inputEvent1 := screen.PollEvent()
 		switch inputEvent1 := inputEvent1.(type) {
 		case *tcell.EventKey:
 			switch inputEvent1.Key() {
+			case tcell.KeyBackspace:
+				i = 6
+				fl = 1
+				break
 			case tcell.KeyRune:
 				inputRune1 := inputEvent1.Rune()
-
-				// 1桁の数字を表示
-				setContents(screen, 33, 10, fmt.Sprintf("数字を選択してください(1~37): %c", inputRune1), tcell.StyleDefault)
+				setContents(screen, 33, 10, fmt.Sprintf("数字を選択してください(1~37): %c", inputRune1), style)
 				screen.Show()
-
-				//time.Sleep(1 * time.Second)
-
-				// 2桁目の数字を取得
 				inputEvent2 := screen.PollEvent()
 				switch inputEvent2 := inputEvent2.(type) {
 				case *tcell.EventKey:
 					switch inputEvent2.Key() {
 					case tcell.KeyRune:
 						inputRune2 := inputEvent2.Rune()
-						setContents(screen, 33, 10, fmt.Sprintf("数字を選択してください(1~37): %c%c", inputRune1, inputRune2), tcell.StyleDefault)
+						setContents(screen, 33, 10, fmt.Sprintf("数字を選択してください(1~37): %c%c", inputRune1, inputRune2), style)
 						screen.Show()
-						es := screen.PollEvent()
-						switch es := es.(type) {
-						case *tcell.EventKey:
-							switch es.Key() {
-							case tcell.KeyEscape, tcell.KeyEnter:
-							}
-						}
-
-						// 1桁の数字の場合、そのまま処理
-						if inputRune2 >= '0' && inputRune2 <= '9' {
-							input, err := strconv.Atoi(string(inputRune1) + string(inputRune2))
-							if err != nil {
-								setContents(screen, 33, 10, "数字の変換に失敗しました", tcell.StyleDefault)
-								setContents(screen, 33, 12, "重複しない数字を選択してください", tcell.StyleDefault)
+						swith(screen)
+						input1, err1 := strconv.Atoi(string(inputRune1))
+						input2, err2 := strconv.Atoi(string(inputRune2))
+						if err1 != nil || err2 != nil {
+							style = tcell.StyleDefault.Foreground(tcell.ColorRed).Bold(true)
+							setContents(screen, 33, 16, "数字の変換に失敗しました", style)
+							setContents(screen, 33, 18, "正しい数字を入力してください", style)
+							screen.Show()
+							swith(screen)
+							i--
+						} else {
+							input := input1*10 + input2
+							if contains(tnumber, input) || input < 1 || input > 37 {
+								style = tcell.StyleDefault.Foreground(tcell.ColorRed).Bold(true)
+								setContents(screen, 33, 16, "数字が重複、もしくは範囲外の数字です。 ", style)
+								setContents(screen, 33, 18, "重複しない数字を選択してください", style)
 								screen.Show()
-								continue
+								i--
+								swith(screen)
+							} else {
+								tnumber[i] = input
+								break
 							}
-
-							// 入力が範囲外の場合や重複している場合の処理
-							for contains(numbers, input) || input < 1 || input > 37 {
-								setContents(screen, 33, 10, "数字が重複、もしくは範囲外の数字です。 ", tcell.StyleDefault)
-								setContents(screen, 33, 12, "重複しない数字を選択してください", tcell.StyleDefault)
-								screen.Show()
-
-								inputEvent := screen.PollEvent()
-								switch inputEvent := inputEvent.(type) {
-								case *tcell.EventKey:
-									switch inputEvent.Key() {
-									case tcell.KeyRune:
-										inputRune2 := inputEvent.Rune()
-
-										// 1桁の数字の場合、そのまま処理
-										if inputRune2 >= '0' && inputRune2 <= '9' {
-											input, err = strconv.Atoi(string(inputRune1) + string(inputRune2))
-											if err != nil {
-												setContents(screen, 33, 10, "数字の変換に失敗しました", tcell.StyleDefault)
-												setContents(screen, 33, 12, "重複しない数字を選択してください", tcell.StyleDefault)
-												screen.Show()
-												continue
-											}
-										}
-									}
-								}
-							}
-							numbers[i] = input
-							break
 						}
 					}
 				}
 			}
 		}
 	}
-	sort.Slice(numbers, func(i, j int) bool {
-		return numbers[i] < numbers[j]
+	sort.Slice(tnumber, func(i, j int) bool {
+		return tnumber[i] < tnumber[j]
 	})
 	lottery := &Lottery{
-		number: numbers,
+		number: tnumber,
 	}
-	if chs(numbers, ab) {
-		setContents(screen, 33, 14, "既に購入した宝くじです。", tcell.StyleDefault)
-		setContents(screen, 33, 16, "別の宝くじを選択してください。", tcell.StyleDefault)
+	if chs(tnumber, ab) && fl == 0 {
+		style = tcell.StyleDefault.Foreground(tcell.ColorRed).Bold(true)
+		setContents(screen, 33, 16, "既に購入した宝くじです", style)
+		setContents(screen, 33, 18, "別の宝くじを選択してください", style)
 		screen.Show()
-		es := screen.PollEvent()
-		switch es := es.(type) {
-		case *tcell.EventKey:
-			switch es.Key() {
-			case tcell.KeyEscape, tcell.KeyEnter:
-			}
-		}
-		//time.Sleep(3 * time.Second)
-		//fmt.Println("既に購入した宝くじです。")
-		//fmt.Println("別の宝くじを選択してください。")
-		ab.CBuyTicket(screen, style)
+		swith(screen)
+		ab.CBuyTicket(screen)
 	} else {
-		ab.numbers = append(ab.numbers, lottery)
-		ab.saveLottery()
+		if !renumber(tnumber) {
+			ab.numbers = append(ab.numbers, lottery)
+			ab.saveLottery()
+		}
 	}
 }
 
@@ -218,7 +191,6 @@ func (ab *BuyLottery) saveLottery() {
 		os.Exit(1)
 	}
 	defer f.Close()
-
 	for _, lottery := range ab.numbers {
 		for _, num := range lottery.number {
 			_, err := fmt.Fprintf(f, "%d ", num)
@@ -237,9 +209,7 @@ func drawLottery() []int {
 
 func Rnumbers() []int {
 	numbers := make([]int, 7)
-
 	for i := 0; i < 7; i++ {
-		// 既に取得した数字と重複しないように生成
 		num := rand.Intn(37) + 1
 		for contains(numbers, num) {
 			num = rand.Intn(37) + 1
@@ -255,7 +225,16 @@ func Rnumbers() []int {
 
 func contains(slice []int, num int) bool {
 	for _, v := range slice {
-		if v == num || 0 > num || num > 38 {
+		if v == num || 1 > num || num > 38 || num == 0 {
+			return true
+		}
+	}
+	return false
+}
+
+func renumber(slice []int) bool {
+	for _, v := range slice {
+		if v == 0 {
 			return true
 		}
 	}
@@ -278,45 +257,6 @@ func chs(slice []int, ab *BuyLottery) bool {
 	return false
 }
 
-func CheckNumber(tickets []*Lottery, lotteryResult []int, screen tcell.Screen, style tcell.Style) {
-	//screen.Clear()
-	l := 0
-	w := 10
-	c := 0
-	var q []int
-	var ns []string
-	//fmt.Println("当選した宝くじ番号:")
-	for _, ticket := range tickets {
-		q = append(q, compare(ticket.number, lotteryResult))
-		//fmt.Println(ticket.number)
-		//pn(q)
-		numbersStr := ""
-		for _, num := range ticket.number {
-			numbersStr += strconv.Itoa(num) + " "
-		}
-		ns = append(ns, numbersStr)
-	}
-	//setContents(screen, 33, s+2, numbersStr, tcell.StyleDefault)
-	for _, m := range ns {
-		w += 2
-		if w == 32 {
-			l += 35
-			w = 12
-		}
-		setContents(screen, l, w, m, tcell.StyleDefault)
-		setContents(screen, l+25, w, pn(q[c]), tcell.StyleDefault)
-		c += 1
-	}
-	screen.Show()
-	es := screen.PollEvent()
-	switch es := es.(type) {
-	case *tcell.EventKey:
-		switch es.Key() {
-		case tcell.KeyEscape, tcell.KeyEnter:
-		}
-	}
-}
-
 func compare(numbers1, numbers2 []int) int {
 	n := 0
 	for i := range numbers1 {
@@ -329,27 +269,69 @@ func compare(numbers1, numbers2 []int) int {
 	return n
 }
 
-func pn(n int) string {
+func CheckNumber(tickets []*Lottery, lotteryResult []int, screen tcell.Screen) {
+	l := 0
+	w := 10
+	c := 0
+	var q []int
+	var ns []string
+	style := tcell.StyleDefault.Foreground(tcell.ColorWhite).Bold(true)
+	for _, ticket := range tickets {
+		q = append(q, compare(ticket.number, lotteryResult))
+		//fmt.Println(ticket.number)
+		//pn(q)
+		numbersStr := ""
+		for _, num := range ticket.number {
+			numbersStr += strconv.Itoa(num) + " "
+		}
+		ns = append(ns, numbersStr)
+	}
+	for _, m := range ns {
+		w += 2
+		if w == 32 {
+			l += 35
+			w = 12
+		}
+		setContents(screen, l, w, m, style)
+		cn, st := pn(q[c])
+		setContents(screen, l+25, w, cn, st)
+		c += 1
+	}
+}
+
+func pn(n int) (string, tcell.Style) {
 	switch n {
 	case 0, 1, 2:
-		return "はずれ"
+		return "はずれ", tcell.StyleDefault.Foreground(tcell.ColorWhite).Bold(true)
 	case 3:
-		return "5等"
+		return "5等", tcell.StyleDefault.Foreground(tcell.ColorBlue).Bold(true)
 	case 4:
-		return "4等"
+		return "4等", tcell.StyleDefault.Foreground(tcell.ColorGreen).Bold(true)
 	case 5:
-		return "3等"
+		return "3等", tcell.StyleDefault.Foreground(tcell.Color94).Bold(true)
 	case 6:
-		return "2等"
+		return "2等", tcell.StyleDefault.Foreground(tcell.ColorSilver).Bold(true)
 	case 7:
-		return "1等"
+		return "1等", tcell.StyleDefault.Foreground(tcell.ColorGold).Bold(true)
 	default:
-		return ""
+		return "", tcell.StyleDefault.Foreground(tcell.ColorWhite).Bold(true)
 	}
 }
 
 func Fclear(filename string) {
-	// ファイルを開く（存在しなければ新しく作成）
 	os.Remove(filename)
 	os.Create(filename)
+}
+
+func swith(screen tcell.Screen) {
+	for {
+		es := screen.PollEvent()
+		switch es := es.(type) {
+		case *tcell.EventKey:
+			switch es.Key() {
+			case tcell.KeyEnter:
+				return
+			}
+		}
+	}
 }
--- main.go
+++ main.go
@@ -4,7 +4,6 @@ import (
 	"fmt"
 	"log"
 	"strconv"
-	"time"
 
 	"github.com/gdamore/tcell"
 	"github.com/mattn/go-runewidth"
@@ -19,6 +18,8 @@ func setContents(screen tcell.Screen, x int, y int, str string, style tcell.Styl
 func main() {
 	filePath := "numbers.txt"
 	buyLottery := NewBuyLottery(filePath)
+	wstyle := tcell.StyleDefault.Foreground(tcell.ColorWhite).Bold(true)
+	rstyle := tcell.StyleDefault.Foreground(tcell.ColorRed).Bold(true)
 	screen, err := tcell.NewScreen()
 	if err != nil {
 		log.Fatal(err)
@@ -27,30 +28,28 @@ func main() {
 		log.Fatal(err)
 	}
 	defer screen.Fini()
-	screen.SetContent(0, 0, '_', nil, tcell.StyleDefault)
+	screen.SetContent(0, 0, '_', nil, wstyle)
 
-	quit := make(chan struct{})
+	lot := make(chan struct{})
 	go func() {
 		for {
 			screen.Clear()
-			setContents(screen, 33, 10, "1. 宝くじを購入する", tcell.StyleDefault)
-			setContents(screen, 33, 12, "2. 購入した宝くじを確認する", tcell.StyleDefault)
-			setContents(screen, 33, 14, "3. 宝くじの抽選を行う", tcell.StyleDefault)
-			setContents(screen, 33, 16, "4. リセット", tcell.StyleDefault)
-			setContents(screen, 33, 18, "5. 終了する", tcell.StyleDefault)
-
-			setContents(screen, 33, 0, "入力：", tcell.StyleDefault)
+			setContents(screen, 33, 10, "1. 宝くじを購入する", wstyle)
+			setContents(screen, 33, 12, "2. 購入した宝くじを確認する", wstyle)
+			setContents(screen, 33, 14, "3. 宝くじの抽選を行う", wstyle)
+			setContents(screen, 33, 16, "4. リセット", wstyle)
+			setContents(screen, 33, 18, "5. 終了する", wstyle)
+			setContents(screen, 33, 0, "入力：", wstyle)
 			screen.Show()
 			ev := screen.PollEvent()
 			switch ev := ev.(type) {
 			case *tcell.EventKey:
 				switch ev.Key() {
 				case tcell.KeyEscape, tcell.KeyEnter, tcell.KeyBackspace:
-					close(quit)
-					return
+					continue
 				case tcell.KeyRune:
 					input := ev.Rune()
-					screen.SetContent(38, 0, ev.Rune(), nil, tcell.StyleDefault)
+					screen.SetContent(39, 0, ev.Rune(), nil, wstyle)
 					screen.Show()
 					es := screen.PollEvent()
 					switch es := es.(type) {
@@ -62,27 +61,24 @@ func main() {
 					}
 					if input == '1' {
 						screen.Clear()
-						setContents(screen, 33, 0, "何枚購入しますか？", tcell.StyleDefault)
+						setContents(screen, 33, 0, "何枚購入しますか(1～9):", wstyle)
 						screen.Show()
 						eq := screen.PollEvent()
 						switch eq := eq.(type) {
 						case *tcell.EventKey:
 							switch eq.Key() {
-							case tcell.KeyBackspace:
+							case tcell.KeyEscape, tcell.KeyEnter, tcell.KeyBackspace:
 								continue
-							case tcell.KeyEscape, tcell.KeyEnter:
-								close(quit)
-								return
 							case tcell.KeyRune:
-								inpute, err := strconv.Atoi(string(eq.Rune()))
-								inputes := strconv.Itoa(inpute)
-								if err != nil {
-									// エラー文
-									//fmt.Println("無効な入力")
-									continue
-								}
-								setContents(screen, 50, 0, inputes, tcell.StyleDefault)
+								screen.SetContent(57, 0, eq.Rune(), nil, wstyle)
 								screen.Show()
+								inpute, err := strconv.Atoi(string(eq.Rune()))
+								if err != nil {
+									setContents(screen, 33, 2, "無効な入力です", rstyle)
+									setContents(screen, 33, 4, "適当な入力でメニュー画面に戻ります", rstyle)
+									screen.Show()
+									swith(screen)
+								}
 								es := screen.PollEvent()
 								switch es := es.(type) {
 								case *tcell.EventKey:
@@ -94,34 +90,36 @@ func main() {
 								for i := 0; i < inpute; i++ {
 									nb := strconv.Itoa(i + 1)
 									screen.Clear()
-									setContents(screen, 33, 10, nb, tcell.StyleDefault)
-									setContents(screen, 34, 10, "枚目", tcell.StyleDefault)
-									setContents(screen, 33, 12, "1. 数字選択", tcell.StyleDefault)
-									setContents(screen, 33, 14, "2. ランダム選択", tcell.StyleDefault)
+									setContents(screen, 33, 10, nb, wstyle)
+									setContents(screen, 34, 10, "枚目:", wstyle)
+									setContents(screen, 33, 12, "1. 数字選択", wstyle)
+									setContents(screen, 33, 14, "2. ランダム選択", wstyle)
 									screen.Show()
 									ex := screen.PollEvent()
 									switch ex := ex.(type) {
 									case *tcell.EventKey:
 										switch ex.Key() {
-										case tcell.KeyEscape, tcell.KeyEnter:
-											close(quit)
-											return
-										case tcell.KeyBackspace:
+										case tcell.KeyEscape, tcell.KeyEnter, tcell.KeyBackspace:
 											continue
 										case tcell.KeyRune:
+											screen.SetContent(40, 10, ex.Rune(), nil, wstyle)
+											screen.Show()
 											inputs, err := strconv.Atoi(string(ex.Rune()))
 											if err != nil {
-												fmt.Println("無効な入力")
-												continue
+												setContents(screen, 33, 16, "無効な選択です", rstyle)
+												screen.Show()
+												swith(screen)
 											}
 											switch inputs {
 											case 1:
-												buyLottery.CBuyTicket(screen, tcell.StyleDefault)
-												//buyLottery.BuyTicket(screen, tcell.StyleDefault)
+												buyLottery.CBuyTicket(screen)
 											case 2:
-												buyLottery.BuyTicket(screen, tcell.StyleDefault)
+												buyLottery.BuyTicket(screen)
 											default:
-												setContents(screen, 33, 16, "無効な選択です", tcell.StyleDefault)
+												setContents(screen, 33, 16, "無効な選択です", rstyle)
+												screen.Show()
+												i--
+												swith(screen)
 											}
 
 										}
@@ -130,28 +128,19 @@ func main() {
 							}
 						}
 
-					}
-					if input == '2' {
+					} else if input == '2' {
 						l := 0
 						w := 10
 						var ns []string
-						//fmt.Println("購入した宝くじ一覧:")
 						screen.Clear()
-						setContents(screen, l, w, "購入した宝くじ一覧:", tcell.StyleDefault)
-						//setContents(screen, 33, 12, "2. 購入した宝くじを確認する", tcell.StyleDefault)
-						//setContents(screen, 33, 14, "3. 宝くじの抽選を行う", tcell.StyleDefault)
-						//setContents(screen, 33, 16, "4. リセット", tcell.StyleDefault)
-						//setContents(screen, 33, 18, "5. 終了する", tcell.StyleDefault)
-						//setContents(screen, 33, 0, "入力：", tcell.StyleDefault)
-						//screen.Show()
+						setContents(screen, l, w, "購入した宝くじ一覧:", wstyle)
+						setContents(screen, l, 34, "エンターキーでメニューに戻ります", wstyle)
 						for _, lottery := range buyLottery.numbers {
-							//fmt.Println(lottery.number)
 							numbersStr := ""
 							for _, num := range lottery.number {
 								numbersStr += strconv.Itoa(num) + " "
 							}
 							ns = append(ns, numbersStr)
-							//setContents(screen, 33, s+2, numbersStr, tcell.StyleDefault)
 						}
 						for _, m := range ns {
 							w += 2
@@ -159,30 +148,20 @@ func main() {
 								l += 30
 								w = 12
 							}
-							setContents(screen, l, w, m, tcell.StyleDefault)
+							setContents(screen, l, w, m, wstyle)
 
 						}
 						screen.Show()
-						es := screen.PollEvent()
-						switch es := es.(type) {
-						case *tcell.EventKey:
-							switch es.Key() {
-							case tcell.KeyEscape, tcell.KeyEnter:
-								continue
-							}
-						}
-					}
-					if input == '3' {
+						swith(screen)
+					} else if input == '3' {
 						screen.Clear()
 						result := drawLottery()
 						//検証用
 						//nresult := []int{10, 11, 12, 13, 14, 15, 16}
 						//result = nresult
-						//
-
-						//setContents(screen, 33, 10, fmt.Sprintf("数字を選択してください(1~37): %c", inputRune1), tcell.StyleDefault)
-						setContents(screen, 0, 10, fmt.Sprintf("抽選結果: %v\n", result), tcell.StyleDefault)
-						CheckNumber(buyLottery.numbers, result, screen, tcell.StyleDefault)
+						setContents(screen, 0, 10, fmt.Sprintf("抽選結果: %v\n", result), wstyle)
+						CheckNumber(buyLottery.numbers, result, screen)
+						setContents(screen, 0, 34, "エンターキーでメニューに戻ります", wstyle)
 						screen.Show()
 						es := screen.PollEvent()
 						switch es := es.(type) {
@@ -192,12 +171,11 @@ func main() {
 								continue
 							}
 						}
-						//CheckNumber(buyLottery.numbers, result, screen, tcell.StyleDefault)
-					}
-					if input == '4' {
+					} else if input == '4' {
 						screen.Clear()
 						Fclear(filePath)
-						setContents(screen, 0, 10, "購入した宝くじをリセットしました。", tcell.StyleDefault)
+						setContents(screen, 0, 10, "購入した宝くじをリセットしました", wstyle)
+						setContents(screen, 0, 12, "エンターキーでメニューに戻ります", wstyle)
 						screen.Show()
 						buyLottery = NewBuyLottery(filePath)
 						es := screen.PollEvent()
@@ -208,16 +186,16 @@ func main() {
 								continue
 							}
 						}
+					} else if input == '5' {
+						close(lot)
+					} else {
+						setContents(screen, 33, 2, "無効な入力です", rstyle)
+						screen.Show()
+						swith(screen)
 					}
-					if input == '5' {
-						close(quit)
-					}
-					//fmt.Print(input)
-					screen.SetContent(40, 0, input, nil, tcell.StyleDefault)
-					time.Sleep(time.Millisecond * 100)
 				}
 			}
 		}
 	}()
-	<-quit
+	<-lot
 }
